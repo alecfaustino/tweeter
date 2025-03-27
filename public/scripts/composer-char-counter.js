30f66b20f27bf4c13e3ed498be696e8970f1998c
Alec Faustino <alecfaustino@outlook.com>
2025-03-26 22:16:27 -0600

    change color when counter value is negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,14 +4,25 @@ $(document).ready(function() {
   $("#tweet-text").on("input", function() {
     // this refers to the tweet text area
     // find the length of the value of the string 
-    let totalCharlength = $(this).val().length;
+    let availableChars = 140 - $(this).val().length;
 
     // counter is not a direct child of text area. 
     // traversing up to form and back down to counter
     // so that only the text area counter is adjusted
+    
+    let counterElement = 
     $(this)
     .closest("form")
-    .find(".counter")
-    .text(140 - totalCharlength);
+    .find(".counter");
+
+    counterElement.text(availableChars);
+
+    if (availableChars < 0) {
+      // add another class so I can select it with CSS
+      counterElement.addClass("not-valid");
+    } else {
+      // remove it if valid char count
+      counterElement.removeClass("not-valid");
+    }
   });
 });

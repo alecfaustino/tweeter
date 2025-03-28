08269670472676afd4141bb3f87f5e7d68be5ec9
Alec Faustino <alecfaustino@outlook.com>
2025-03-28 08:12:19 -0600

    set up createTweetElement function. Unfinished

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+  
+  const createTweetElement = function(tweetObj) {
+    
+    return $(`<article class="tweet">Testing</article>`);
+  }
+  
+  const $tweet = createTweetElement(tweetData);
+  
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
